c9ab58dc8601f60df79eb81bac5d553f5b8edcf9
Logeshwaran <logiloga357@gmail.com>
2021-11-10 15:08:18 +0530

    All Ui Completed

--- app/src/main/java/com/example/lchat/homepage.java
+++ app/src/main/java/com/example/lchat/homepage.java
@@ -1,14 +1,48 @@
 package com.example.lchat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class homepage extends AppCompatActivity {
+
+
+    BottomNavigationView bottomnav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+        bottomnav = findViewById(R.id.bottom_navigation);
+        bottomnav.setOnNavigationItemSelectedListener(navlistner);
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_nav,new chat()).commit();
+
+
+
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener navlistner =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+                    switch (item.getItemId()){
+                        case R.id.chat:
+                            selectedFragment = new chat();
+                            break;
+                        case R.id.online:
+                            selectedFragment = new online();
+                            break;
+                        case R.id.last:
+                            selectedFragment = new lastmes();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frame_nav,selectedFragment).commit();
+                    return true;
+                }
+            };
 }
--- app/src/main/java/com/example/lchat/loginpage.java
+++ app/src/main/java/com/example/lchat/loginpage.java
@@ -1,28 +1,95 @@
 package com.example.lchat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class loginpage extends AppCompatActivity {
-    Button btn;
+    Button login;
+    EditText inputEmail,inputPassword;
+    String emailPattern ="[a-zA-Z0-9._-]+@[a-z]+//.+[a-z]+";
+    ProgressDialog progressDialog;
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loginpage);
-        btn = findViewById(R.id.login);
-        btn.setOnClickListener(new View.OnClickListener() {
+        login = findViewById(R.id.login);
+        inputEmail = findViewById(R.id.inputEmail);
+        inputPassword = findViewById(R.id.inputPassword);
+        progressDialog = new ProgressDialog(this);
+        mAuth =FirebaseAuth.getInstance();
+        mUser=mAuth.getCurrentUser();
+
+
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(),homepage.class);
-                startActivity(i);
-
+                PerformLogin();
             }
         });
+    }
+
+    private void PerformLogin() {
+        String email = inputEmail.getText().toString();
+        String password = inputPassword.getText().toString();
+
+        if (email.matches(emailPattern)) {
+            inputEmail.setError("Enter Valid Email");
+        } else if (password.isEmpty() || password.length() < 6) {
+            inputPassword.setError("Enter Correct Password");
+        } else {
+            progressDialog.setMessage("Logging in.....");
+            progressDialog.setTitle("Login");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+
+                        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+                        LocalDateTime now = LocalDateTime.now();
+
+                        progressDialog.dismiss();
+                        sendUserToNextActivity();
+                        Toast.makeText(loginpage.this, "Login Successfull", Toast.LENGTH_SHORT).show();
+                    }else
+                    {
+                        progressDialog.dismiss();
+                        Toast.makeText(loginpage.this, ""+task.getException(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+
+    }
+
+    private void sendUserToNextActivity() {
+        Intent intent=new Intent(loginpage.this,homepage.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+
     }
 }
